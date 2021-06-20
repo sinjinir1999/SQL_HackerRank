cc8653c73ae1c5de9654aa35e07693e3749738d7
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-06-21 01:06:39 +0530

    Create 17.sql

---
+++ 17.sql
@@ -0,0 +1,5 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '^[AEIOU]' AND CITY NOT REGEXP '[AEIOU]$';
