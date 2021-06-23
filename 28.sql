3ef4010d9ba36f6add7cd6ff3fd7d29d9ae8923b
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-06-23 10:31:49 +0530

    Update 28.sql

--- 28.sql
+++ 28.sql
@@ -1,5 +1,5 @@
 /*
-Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7800 and less than 137.2345. Truncate your answer to  decimal places.
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7800 and less than 137.2345. Truncate your answer to 4 decimal places.
 */
 
 SELECT ROUND(SUM(LAT_N),4) AS LAT FROM STATION 
